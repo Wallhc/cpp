7f79964739683b9adf16b7275613b66964d6b07d
Wallhc <936338792@qq.com>
2018-09-08 16:15:59 +0800

    forward_list_erase

--- vscodec++/primer7_1.cpp
+++ vscodec++/primer7_1.cpp
@@ -1,20 +1,37 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<list>
+#include<forward_list>
 using namespace std;
-class Person{
-    public:
-    Person() = default;
-    Person(const string n,const string ad): name(n),address(ad){}
-    string name;
-    string address;
-    private:
-    string GetName() const { return name;}
-    string GetAddress() const {return address;}
-    void PrintName() const { cout<<name<<endl; }
-    void PrintAddress() const { cout<<address<<endl; }
-};
 
-int main{
-    Person p = new Person();
 
-}
+int main()
+{
+    int ia[] = {0,1,1,2,3,5,8,13,21,55,89};
+    forward_list<int> fl(ia,ia+11);
+
+    forward_list<int>::iterator flbi = fl.before_begin();
+    forward_list<int>::iterator fli = fl.begin();
+    while(fli!=fl.end())
+    {
+        if((*fli)%2)
+        {
+            fli = fl.erase_after(flbi);
+        }
+        else
+        {
+            fli++;
+            flbi++;
+        }
+        
+    } 
+    fli = fl.begin();
+    while(fli!=fl.end())
+    {
+        cout<<*fli<<" ";
+        fli++;
+    }
+
+    system("pause");
+}   
