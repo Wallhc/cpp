2705cd4108c6bafbdf069cecbdb6ac4113149e88
Wallhc <936338792@qq.com>
2018-09-14 14:39:56 +0800

    11.12

---
+++ vscodec++/primer_11_12.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<fstream>
+#include<cctype>
+using namespace std;
+
+int main()
+{
+    vector<pair<string,int>> v;
+
+}
